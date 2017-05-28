74a97da46f7abfca0756d763539994e751737c65
Blake Schwartz <blakehschwartz@gmail.com>
2017-05-28 09:46:45 -0700

    Reorganize files, adds basic event handling/event object access examples

---
+++ vuejs2/events.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Vue({
+        el: '#app',
+        data: {
+            counter: 0,
+            x: 0,
+            y: 0
+        },
+        methods: {
+            increase: function(step, event) {
+                this.counter += step;
+                // Use event object here to access DOM elements (if desired)
+                event.target.nextElementSibling.style.backgroundColor = 'red';
+            },
+            updateCoordinates: function(event) {
+            // Native event object is accessible thru any v-on binding
+                this.x = event.clientX;
+                this.y = event.clientY;
+            }
+        }
+    });
+});
