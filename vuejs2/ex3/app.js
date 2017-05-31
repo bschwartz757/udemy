afc479bb074e3b42bcdf3b66bbba233392281f6a
Blake Schwartz <blakehschwartz@gmail.com>
2017-05-30 18:41:57 -0700

    finish renaming files

---
+++ vuejs2/ex3/app.js
@@ -0,0 +1,19 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('loaded!');
+    new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+        methods: {
+            showAlert: function (event) {
+                alert(`${event.target.outerHTML} was clicked!`);
+            },
+            updateValue: function (event) {
+                this.value = event.target.value;
+            }
+        }
+    });
+
+});
