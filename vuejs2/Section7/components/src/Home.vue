1202e6212b0ca27ec3cbb78a9090f1e044fa1f75
Blake Schwartz <blakehschwartz@gmail.com>
2017-06-01 22:21:02 -0700

    Section7: Intro to Components (uses webpack dev setup)

---
+++ vuejs2/Section7/components/src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--We can use v-for inside templates-->
+    <!--But, need to wrap it inside a div (because we can only have one root element
+    inside template)-->
+    <div>
+        <app-server-status v-for="server in 5"></app-server-status>
+    </div>
+</template>
+
+<script>
+    // Import from another component file
+    import ServerStatus from './ServerStatus.vue';
+    export default {
+        // Register component locally
+        components: {
+            'app-server-status': ServerStatus
+        }
+    }
+</script>
---
+++ vuejs2/Section7/components/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+    <!--Template can contain only ONE root element-->
+    <div>
+        <p>Server Status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                status: 'Critical'
+            };
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
