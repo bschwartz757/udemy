b4c91dc02d2e4a1e6605a5cbec103d3f3a84f1f2
Blake Schwartz <blakehschwartz@gmail.com>
2017-06-01 23:52:14 -0700

    Adds ex6 finished code: splits App.vue file into smaller components

---
+++ vuejs2/Section7/ex6/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <page-header></page-header>
+        <hr>
+        <div class="row">
+            <server-list></server-list>
+            <server-details></server-details>
+        </div>
+        <hr>
+        <page-footer></page-footer>
+    </div>
+</template>
+
+<script>
+
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import ServerDetails from './ServerDetails.vue';
+import ServerList from './ServerList.vue';
+
+export default {
+    components: {
+        'page-header': Header,
+        'page-footer': Footer,
+        'server-details': ServerDetails,
+        'server-list': ServerList
+    }
+}
+
+</script>
+
+<style>
+
+</style>
