bdea41cf6eff54ef3fb73aef25d30f18d881c6eb
Blake Schwartz <blakehschwartz@gmail.com>
2017-06-02 12:37:36 -0700

    Adds Section8: Communicating between components (examples)

---
+++ vuejs2/Section8/component_communication/src/components/User.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change Name</button>
+        <p>Name is {{ name }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!--IMPORTANT: need the colon and the attribute identifier must 
+                match what we're trying to pass in the child component (UserDetail.vue)-->
+                <!--@nameWasReset is a custom is a custom event we emit from the child
+                component to communicate with the parent component. This is sweet.-->
+                <!--$event is the custom event we emitted from the child component-->
+                <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+                <!--Passing 1 here will throw a validation error (see UserDetail.vue)-->
+                <!--<app-user-detail :name="1"></app-user-detail>-->
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data() {
+            return {
+                name: 'Blake'
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Stacey'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ vuejs2/Section8/component_communication/src/components/UserDetail.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>Reverse Name: {{ reverseName(1) }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    // The elements we specify in the props array must match what we set up 
+    // in the parent component (User.vue)
+    export default {
+        // Props can be an array:
+        // props: ['name'],
+        // Or an object:
+        props: {
+            // Validation - can specify multiple types - COOL!
+            name: {
+                type: [String, Array],
+                required: true
+            }
+            // You can use a default value instead of 'required'
+            // If default is an object, use a function
+            // default: 'Blake'
+        },
+        // We can also export methods that are used in the parent component
+        methods: {
+            reverseName() {
+                return this.name.split('').reverse('').join('');
+            },
+            resetName() {
+                this.name = 'Blake';
+                // We can pass a custom event to the parent component by accessing
+                // the '$emit' event property of the Vue instance... We're not in 
+                // jQuery land anymore
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
