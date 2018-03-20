b2d22822853a7ded868d8aecbe5a57baa2ebfde7
Schwartz, Blake <Blake.H.Schwartz.-ND@disney.com>
2018-03-19 22:31:15 -0700

    adds lifecycle methods

--- react-complete-guide/src/components/People/People.js
+++ react-complete-guide/src/components/People/People.js
@@ -1,51 +1,53 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person'
 
-class People extends Component {
+class People extends PureComponent {
 
-	constructor(props) {
-		super(props);
-		console.log('[People.js] inside constructor', props)
-	}
+  constructor(props) {
+    super(props);
+    console.log('[People.js] inside constructor', props)
+  }
 
-	componentWillMount() {
-		console.log('[People.js] inside componentWillMount')
-	}
+  componentWillMount() {
+    console.log('[People.js] inside componentWillMount')
+  }
 
-	componentDidMount() {
-		console.log('[People.js] inside componentDidMount')
-	}
+  componentDidMount() {
+    console.log('[People.js] inside componentDidMount')
+  }
 
-	componentWillReceiveProps(nextProps) {
-		console.log('[Update People.js] inside componentWillReceiveProps', nextProps)
-	}
+  componentWillReceiveProps(nextProps) {
+    console.log('[Update People.js] inside componentWillReceiveProps', nextProps)
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log('[Update People.js] inside shouldComponentUpdate', nextProps, nextState)
-		return nextProps.people !== this.props.people
-	}
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Update People.js] inside shouldComponentUpdate', nextProps, nextState)
+  //   return nextProps.people !== this.props.people ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.click !== this.props.click
+  // }
 
-	componentWillUpdate(nextProps, nextState) {
-		console.log('[Update People.js] inside componentWillUpdate', nextProps, nextState)
-	}
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Update People.js] inside componentWillUpdate', nextProps, nextState)
+  }
 
-	componentDidUpdate() {
-		console.log('[Update People.js] inside componentDidUpdate')
-	}
+  componentDidUpdate() {
+    console.log('[Update People.js] inside componentDidUpdate')
+  }
 
-	render() {
-		console.log('[People.js] inside render')
-		return this.props.people.map(
-			(person, index) => {
-				return <Person
-					click={() => this.props.click(index)}
-					key={person.id}
-					name={person.name}
-					age={person.age}
-					change={(event) => this.props.change(event, person.id)}
-				/>
-			})
-	}
+  render() {
+    console.log('[People.js] inside render')
+    return this.props.people.map(
+      (person, index) => {
+        return <Person
+          click={() => this.props.click(index)}
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          change={(event) => this.props.change(event, person.id)}
+        />
+      })
+  }
 
 }
 
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,91 +1,106 @@
 import React, {
-	Component
+  PureComponent
 } from 'react';
 import styles from './App.css';
 import People from '../components/People/People';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		console.log('[App.js] inside constructor', props)
-	}
+// PureComponent checks for shallow differences when updating components (the same behavior as shouldComponentUpdate)
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] inside constructor', props)
+    this.state = {
+      people: [
+        { id: 'asdf', name: "Blake", age: 35 },
+        { id: 'Fvc', name: "Stacey", age: 30 },
+        { id: 'qtqert', name: "Tilly", age: 4 },
+        { id: 'poiu', name: "Penny", age: 63 }
+      ],
+      showPeople: false
+    }
+  }
 
-	componentWillMount() {
-		console.log('[App.js] inside componentWillMount')
-	}
+  componentWillMount() {
+    console.log('[App.js] inside componentWillMount')
+  }
 
-	componentDidMount() {
-		console.log('[App.js] inside componentDidMount')
-	}
+  componentDidMount() {
+    console.log('[App.js] inside componentDidMount')
+  }
 
-	state = {
-		people: [
-			{ id: 'asdf', name: "Blake", age: 35 },
-			{ id: 'Fvc', name: "Stacey", age: 30 },
-			{ id: 'qtqert', name: "Tilly", age: 4 },
-			{ id: 'poiu', name: "Penny", age: 63 }
-		],
-		showPeople: false
-	}
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Update App.js] inside shouldComponentUpdate', nextProps, nextState)
+  //   return nextState.people !== this.state.people ||
+  //     nextState.showPeople !== this.state.showPeople
+  // }
 
-	nameChangeHandler = (event, id) => {
-		const personIndex = this.state.people.findIndex(p => {
-			return p.id === id
-		})
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Update App.js] inside componentWillUpdate', nextProps, nextState)
+  }
 
-		// spread operator here returns a copy of the original person item
-		// equivalent to this.state.people[personIndex].slice()
-		// this is necessary to avoid mutating the original person array
-		const person = {
-			...this.state.people[personIndex]
-		}
+  componentDidUpdate() {
+    console.log('[Update App.js] inside componentDidUpdate')
+  }
 
-		person.name = event.target.value
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.people.findIndex(p => {
+      return p.id === id
+    })
 
-		// gets a new copy of the original state array
-		const people = [...this.state.people]
-		people[personIndex] = person
+    // spread operator here returns a copy of the original person item
+    // equivalent to this.state.people[personIndex].slice()
+    // this is necessary to avoid mutating the original person array
+    const person = {
+      ...this.state.people[personIndex]
+    }
 
-		this.setState({
-			people: people
-		})
-	}
+    person.name = event.target.value
 
-	deletePersonHandler = (personIndex) => {
-		// gets a new copy of the original state array		
-		const people = [...this.state.people]
-		people.splice(personIndex, 1)
-		this.setState({ people: people })
-	}
+    // gets a new copy of the original state array
+    const people = [...this.state.people]
+    people[personIndex] = person
 
-	toggleListHandler = () => {
-		const show = this.state.showPeople
-		this.setState({ showPeople: !show })
-	}
+    this.setState({
+      people: people
+    })
+  }
 
-	render() {
-		console.log('[App.js] inside render()')
-		let peopleList
+  deletePersonHandler = (personIndex) => {
+    // gets a new copy of the original state array		
+    const people = [...this.state.people]
+    people.splice(personIndex, 1)
+    this.setState({ people: people })
+  }
 
-		if (this.state.showPeople) {
-			peopleList = <People
-				people={this.state.people}
-				click={this.deletePersonHandler}
-				change={this.nameChangeHandler} />
-		}
+  toggleListHandler = () => {
+    const show = this.state.showPeople
+    this.setState({ showPeople: !show })
+  }
 
-		return (
-			<div className={styles.App}>
-				<Cockpit
-					appTitle={this.props.title}
-					showPeople={this.state.showPeople}
-					people={this.state.people}
-					click={this.toggleListHandler} />
-				{peopleList}
-			</div>
-		);
-	}
+  render() {
+    console.log('[App.js] inside render()')
+    let peopleList
+
+    if (this.state.showPeople) {
+      peopleList = <People
+        people={this.state.people}
+        click={this.deletePersonHandler}
+        change={this.nameChangeHandler} />
+    }
+
+    return (
+      <div className={styles.App}>
+        <button onClick={() => { this.setState({ showPeople: true }) }}>Show People</button>
+        <Cockpit
+          appTitle={this.props.title}
+          showPeople={this.state.showPeople}
+          people={this.state.people}
+          click={this.toggleListHandler} />
+        {peopleList}
+      </div>
+    );
+  }
 }
 
 export default App;
